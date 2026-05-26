82142061bbce78168f6fc04877098ede70620400
Gaurang Raut <Gaurangraut2004@gmail.com>
2026-05-26 23:31:41 +0530

    fix: relocate imports to top-level for ES module compliance and fix Render crash

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+
 import userroutes from "./routes/auth.js";
 import videoroutes from "./routes/video.js";
 import likeroutes from "./routes/like.js";
@@ -10,9 +14,9 @@ import watchlaterroutes from "./routes/watchlater.js";
 import historyrroutes from "./routes/history.js";
 import commentroutes from "./routes/comment.js";
 import paymentroutes from "./routes/payment.js";
+
 dotenv.config();
 const app = express();
-import path from "path";
 app.use(cors());
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -28,11 +32,8 @@ app.use("/watch", watchlaterroutes);
 app.use("/history", historyrroutes);
 app.use("/comment", commentroutes);
 app.use("/payment", paymentroutes);
+
 const PORT = process.env.PORT || 5000;
-import { Server } from "socket.io";
-
-import http from "http";
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
