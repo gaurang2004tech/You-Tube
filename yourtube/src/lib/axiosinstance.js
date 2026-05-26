1ff343764f26cb38b5f12efa827c2edc41427c12
Gaurang Raut <Gaurangraut2004@gmail.com>
2026-05-27 00:13:40 +0530

    debug: add axios interceptors to track api calls in browser console

--- yourtube/src/lib/axiosinstance.js
+++ yourtube/src/lib/axiosinstance.js
@@ -1,6 +1,22 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  console.log(`AXIO_REQ: ${config.method?.toUpperCase()} ${config.baseURL || ""}${config.url}`);
+  return config;
+}, (error) => {
+  console.error("AXIO_REQ_ERR:", error);
+  return Promise.reject(error);
+});
+
+axiosInstance.interceptors.response.use((response) => {
+  console.log(`AXIO_RES: ${response.status} from ${response.config.url}`);
+  return response;
+}, (error) => {
+  console.error("AXIO_RES_ERR:", error.response?.status, error.message);
+  return Promise.reject(error);
+});
+
 export default axiosInstance;
