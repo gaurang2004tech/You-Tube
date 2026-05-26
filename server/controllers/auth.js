6b2bcdac7921707bec20b459a32e011d99e1c24c
Gaurang Raut <Gaurangraut2004@gmail.com>
2026-05-27 00:30:29 +0530

    fix: switch to SMTP port 587 to resolve Render connection timeouts

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -9,7 +9,9 @@ const otpStore = new Map();
 // Nodemailer setup
 const getTransporter = () =>
   nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // upgrade later with STARTTLS
     auth: {
       user: process.env.NODEMAILER_USER,
       pass: process.env.NODEMAILER_PASS,
